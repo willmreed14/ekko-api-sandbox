7421049555d8b883bc9d23eab6e86052b76d9f47
William Reed <willmreed14@gmail.com>
2025-06-16 17:48:34 -0400

    clear vite UI template

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import { type FC } from 'react';
 
 const Header: FC = () => {
   return (
-    <header className="bg-slate-800 border-b border-slate-700 p-4">
+    <header className="p-4">
       <div className="container mx-auto">
         <h1 className="text-xl font-bold text-white">Ekko API Sandbox</h1>
       </div>
