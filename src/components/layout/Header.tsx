08d21a2e717d1665e91007a1d9eeab05a1bbe990
William Reed <willmreed14@gmail.com>
2025-06-16 17:46:26 -0400

    Create a basic Header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,2 +1,15 @@
 /* Header Component */
 
+import { type FC } from 'react';
+
+const Header: FC = () => {
+  return (
+    <header className="bg-slate-800 border-b border-slate-700 p-4">
+      <div className="container mx-auto">
+        <h1 className="text-xl font-bold text-white">Ekko API Sandbox</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
