ab5aa61bc702547e5916e6d6dfe3df27af14e640
William Reed <willmreed14@gmail.com>
2025-06-27 19:58:31 -0400

    Configure app.tsx for react-router SPA

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css';
 import Header from './components/layout/Header.tsx';
 import ApiSandboxLayout from './components/layout/ApiSandboxLayout.tsx';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <ApiSandboxLayout />
-    </>
+      <div className="flex flex-1">
+        {/* Navigation sidebar will go here */}
+        <div className="w-64 bg-gray-50 border-r border-gray-200 p-4">
+          <p className="text-gray-500 text-sm font-medium">API SECTIONS (nav coming soon)</p>
+        </div>
+
+        {/* Main content area with Outlet for child routes */}
+        <main className="flex-1 p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
   );
 }
 
