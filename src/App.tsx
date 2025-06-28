7670f9b9f47d15aba68fbe9113b892bfa9b09d12
William Reed <willmreed14@gmail.com>
2025-06-27 20:47:49 -0400

    Display navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/layout/Header.tsx';
-import ApiSandboxLayout from './components/layout/ApiSandboxLayout.tsx';
+import Navigation from './components/layout/Navigation.tsx';
 import { Outlet } from 'react-router-dom';
 
 function App() {
@@ -8,10 +8,7 @@ function App() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="flex flex-1">
-        {/* Navigation sidebar will go here */}
-        <div className="w-64 bg-gray-50 border-r border-gray-200 p-4">
-          <p className="text-gray-500 text-sm font-medium">API SECTIONS (nav coming soon)</p>
-        </div>
+        <Navigation />
 
         {/* Main content area with Outlet for child routes */}
         <main className="flex-1 p-6">
