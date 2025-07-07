336f754730ded1d8a6f4eb2a7e0234adedd0bee7
William Reed <willmreed14@gmail.com>
2025-07-07 17:28:38 -0400

    Make main content scrollable within, let nav and header stay fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-screen">
+      {/* Fixed header */}
       <Header />
-      <div className="flex flex-1">
+
+      {/* Main content area w/ fixed nav and scrollable content */}
+      <div className="flex flex-1 overflow-hidden">
         <Navigation />
 
-        {/* Main content area with Outlet for child routes */}
-        <main className="flex-1 p-6">
+        {/* Scrollable main content area */}
+        <main className="flex-1 overflow-y-auto p-6">
           <Outlet />
         </main>
       </div>
