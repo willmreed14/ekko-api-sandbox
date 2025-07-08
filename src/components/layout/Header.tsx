8f58b25783ce556995bfcff24218023831b4ee89
William Reed <willmreed14@gmail.com>
2025-07-08 13:17:08 -0400

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import { type FC } from 'react';
 
 const Header: FC = () => {
   return (
-    <header className="p-4">
+    <header className="p-4 border-b border-solid border-gray-700">
       <div className="container mx-auto">
         <h1 className="text-xl font-bold text-white text-center">Ekko API Sandbox</h1>
       </div>
