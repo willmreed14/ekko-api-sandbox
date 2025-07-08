e9216fd8f383e536d9d15bf7c7dfb56ddd2e1d26
William Reed <willmreed14@gmail.com>
2025-07-08 13:25:20 -0400

    Restructure layout from vertical stack to horizontal split
    Allow nav component to extend the full height of the viewport

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="flex flex-col h-screen">
-      {/* Fixed header */}
-      <Header />
+    <div className="flex h-screen">
+      {/* Navigation that extends full height */}
+      <Navigation />
 
-      {/* Main content area w/ fixed nav and scrollable content */}
-      <div className="flex flex-1 overflow-hidden">
-        <Navigation />
+      {/* Right side content with header and main content */}
+      <div className="flex flex-col flex-1">
+        {/* Fixed header */}
+        <Header />
 
         {/* Scrollable main content area */}
         <main className="flex-1 overflow-y-auto p-6">
